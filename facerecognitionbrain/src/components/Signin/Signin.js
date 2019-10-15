15e6cf387f85235dfc505051614fcf41507c7e61
Shane M <shaneSC@gmail.com>
2019-10-15 18:26:51 +0100

    cleaned up

--- facerecognitionbrain/src/components/Signin/Signin.js
+++ facerecognitionbrain/src/components/Signin/Signin.js
@@ -4,15 +4,15 @@ const Signin = ({ onRouteChange }) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-two-thirds mw6 shadow-5 center pa2">
       <main>
-        <form className="measure">
+        <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" for="password">Password</label>
+              <label className="db fw6 lh-copy f6" htmlFor="password" autoComplete="current-password">Password</label>
               <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
             </div>
           </fieldset>
@@ -27,7 +27,7 @@ const Signin = ({ onRouteChange }) => {
           <div className="lh-copy mt3">
             <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
