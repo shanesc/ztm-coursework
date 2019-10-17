2b3708c916b5e23008c5356368db408e20279875
Shane M <shaneSC@gmail.com>
2019-10-17 20:44:33 +0100

    added express.js

--- node/server.js
+++ node/server.js
@@ -1,16 +1,21 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-  // console.log('headers', request.headers);
-  console.log('method', request.method);
-  console.log('url', request.url);
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('getting root');
+});
+
+app.get('/profile', (req, res) => {
+  res.send('getting profile');
+});
+
+app.post('/profile', (req, res) => {
   const user = {
     name: 'Shane',
     hobby: 'Climbing',
   }
-
-  response.setHeader('Content-Type', 'application/json');
-  response.end(JSON.stringify(user));
+  res.send(user);
 });
 
-server.listen(3000);
+app.listen(3000);
