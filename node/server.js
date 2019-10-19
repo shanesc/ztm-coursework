951dc96b893c9f25602cb9f652322570eaeb5457
Shane M <shaneSC@gmail.com>
2019-10-19 08:50:40 +0100

    commit

--- node/server.js
+++ node/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('getting root');
@@ -11,11 +15,12 @@ app.get('/profile', (req, res) => {
 });
 
 app.post('/profile', (req, res) => {
+  console.log(req.body);
   const user = {
     name: 'Shane',
     hobby: 'Climbing',
   }
-  res.send(user);
+  res.send('success');
 });
 
 app.listen(3000);
