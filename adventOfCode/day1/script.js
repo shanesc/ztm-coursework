238426367907cebf9218ee2b4eaa2aa047ff2e99
Shane M <shaneSC@gmail.com>
2019-10-20 11:03:44 +0100

    added question2 to Day 1

--- adventOfCode/day1/script.js
+++ adventOfCode/day1/script.js
@@ -1,20 +1,46 @@
 const fs = require('fs');
 
-fs.readFile('./instructions.txt', (err, data) => {
-  console.time('challenge');
-  if (err) {
-    console.log('Error');
-  }
+// What floor does Santa end up on?
+function question1() {
+  fs.readFile('./instructions.txt', (err, data) => {
+    console.time('challenge1');
 
-  const str = data.toString();
-  let floor = 0;
-  for (let i = 0; i < str.length; i++) {
-    ((str[i] === '(') ? floor++ : floor--);
-    if (floor === -1) {
-      console.log('Position in Basement: ', i+1);
-    }
-  }
-  console.log('Final floor: ', floor);
-  console.timeEnd('challenge');
-});
+    const str = data.toString();
+    const directionsArray = str.split('');
+    const floor = directionsArray.reduce((acc, currVal) => {
+      if (currVal === '(') {
+        return acc += 1;
+      } else if (currVal === ')') {
+        return acc -= 1;
+      }
+    }, 0)
+    console.log('Final floor: ', floor);
+    console.timeEnd('challenge1');
+  });
+}
+question1();
+// When does Santa first enter the basement?
 
+function question2() {
+  fs.readFile('./instructions.txt', (err, data) => {
+    console.time('challenge2');
+
+    const str = data.toString();
+    const directionsArray = str.split('');
+    let accumulator = 0;
+    let directionCounter = 0;
+    const floor = directionsArray.some((currentValue) => {
+      if (currentValue === '(') {
+         accumulator += 1;
+      } else if (currentValue === ')') {
+         accumulator -= 1;
+      }
+      directionCounter++;
+      return (accumulator < 0);
+    })
+    console.log('Position in Basement: ', directionCounter);
+    console.timeEnd('challenge2');
+  });
+}
+
+question2();
