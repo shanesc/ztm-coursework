fb37ed56616021d6bb7d449adccfef3966b0f3a6
Shane M <shaneSC@gmail.com>
2019-10-22 17:05:59 +0100

    added /image route

--- facerecognitionbrain-api/server.js
+++ facerecognitionbrain-api/server.js
@@ -69,17 +69,22 @@ app.get('/profile/:id', (req, res) => {
     res.status(404).json('user not found');
   };
 });
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(404).json('user not found');
+  };
+})
   
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
-
-/*
-various endpoints:
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT = user
-
-*/
